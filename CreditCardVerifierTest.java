32d79c70d64574f651fb24676ab5c7490d4cb1a9
AreeyokaJ <97417918+AreeyokaJ@users.noreply.github.com>
2024-02-25 19:50:04 -0500

    Update CreditCardVerifierTest.java

--- CreditCardVerifierTest.java
+++ CreditCardVerifierTest.java
@@ -4,21 +4,18 @@ class CreditCardVerifierTest{
     public static void main(String[] args){
  
        try{
-        //create creditCardVerifierObject, this object will ask user for credit card number 
-        //and go through credit card verification project  
+        /*create creditCardVerifierObject, this object will ask user for credit card number 
+        and go through credit card verification project */ 
         CreditCardVerifier creditCardVerifier = new CreditCardVerifier(); 
        }
 
-       //if user inputs an invalid card input, i.e. the input has other characters than digits or did not input exactly 16 digits 
-       //method will throw InvalidCreditCardInputException, 
+       /*if user inputs an invalid card input, i.e. the input has other characters than digits or did not input exactly 16 digits 
+       method will throw InvalidCreditCardInputException, */
        catch(InvalidCreditCardInputException e){
             //print message explaining the reason for invalidCreditCardInputException
             System.out.println(e.getMessage());
        }
      
     }
-
-    
-
   
 }
